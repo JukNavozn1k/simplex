bb51235feebc0517d3b891fe1855b80c792aac42
JukNavozn1k <JukNavozn1k>
2024-10-13 22:12:46 +0300

    extracting

--- src/alg.py
+++ src/alg.py
@@ -1,5 +1,6 @@
 import math
-def simplex(A,b,F): 
+
+def simplex(A, b, F):
     # Приведение неравенств к равенствам
     for i in range(len(A)):
         for j in range(len(A)):
@@ -11,32 +12,57 @@ def simplex(A,b,F):
                     b[i] *= -1
             else: 
                 A[i].append(0)
-    # Приведение неравенств к равенствам
-   # Крафт симплекс таблицы 
+    
+    # Добавление столбца b в матрицу A
     for i in range(len(A)):
         A[i].append(b[i])
-    F = [-1*f for f in F]
-    F += [0]*(len(A)+1)
-    A.append(F)
+
+    # Преобразование целевой функции F
+    F = [-1 * f for f in F]
+    F += [0] * (len(A[0]) - len(F))  # Добавляем дополнительные нули
+    A.append(F)  # Добавляем целевую функцию в таблицу
+
+    while min(A[-1][:-1]) < 0:  # Пока есть отрицательные элементы в строке F
+        # Находим опорный столбец
+        pivot_col = A[-1][:-1].index(min(A[-1][:-1]))
+
+        # Формируем столбец для вычисления отношений
+        column = [A[i][pivot_col] for i in range(len(A) - 1)]
+        
+        # Проверка на то, что задача неограничена
+        if max(column) <= 0:
+            raise Exception('Задача неограничена')
+
+        # Вычисляем отношения (отношение свободного члена к элементам столбца)
+        ratios = [A[i][-1] / column[i] if column[i] > 0 else math.inf for i in range(len(column))]
+
+        if min(ratios) == math.inf:
+            raise Exception('Ошибка: Решения нет')
+
+        # Находим опорную строку
+        pivot_row = ratios.index(min(ratios))
+
+        # Опорный элемент
+        pivot = A[pivot_row][pivot_col]
+
+        # Обновление опорной строки (делим на опорный элемент)
+        A[pivot_row] = [x / pivot for x in A[pivot_row]]
+
+        # Обновление остальных строк
+        for i in range(len(A)):
+            if i != pivot_row:
+                factor = A[i][pivot_col]
+                A[i] = [A[i][j] - factor * A[pivot_row][j] for j in range(len(A[0]))]
+
+    # Оптимальное решение находится в последнем столбце
+    solution = [0] * (len(A[0]) - 1)
+    for i in range(len(A) - 1):
+        if 1 in A[i][:-1]:
+            col_idx = A[i].index(1)
+            if all(A[j][col_idx] == 0 for j in range(len(A)) if j != i):
+                solution[col_idx] = A[i][-1]
     
-   
-    while min(A[-1][:-1]) < 0: 
-  
-        pivot_col = F.index(min(A[-1][:-1]))
-        column = [A[i][pivot_col] for i in range(len(A)-1)]
-        
-        # Проверка на то, что задача неограничена
-        if max([A[i][pivot_col] for i in range(len(A))]) <= 0: 
-            raise Exception('Задача неограничена ')
-        # Проверка на то, что задача неограничена
-
-        ratios = [b[i] / column[i] if b[i] > 0 and column[i] > 0 else math.inf for i in range(len(column))]
-        if min(ratios) == math.inf: raise Exception('Ошибка: Решения нет')
-        pivot_row = ratios.index(min(ratios))   
-
-
-        
-
+    return solution, A[-1][-1]  # Возвращаем оптимальное решение и значение целевой функции
 
 
 
@@ -45,7 +71,7 @@ def simplex(A,b,F):
 # b свободные члены
 # F целевая функция
 
-A = [[1,2],[2,5]]
+A = [[1,2],[2,123]]
 b = [1,1]
 F  = [1,15]
-simplex(A,b,F)
+print(simplex(A,b,F))
