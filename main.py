c62c8eb3789fbad8bc68ff44df2fc74586ee2188
gleb <deffender1337@gmail.com>
2025-05-13 02:59:46 +0300

    refactor: remove license display and update tab structure in main interface

--- main.py
+++ main.py
@@ -31,36 +31,11 @@ def show_instructions():
     ```
     """)
 
-def show_license():
-    st.title("Лицензия")
-    st.markdown("""
-    MIT License
-
-    Copyright (c) 2024
-
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in all
-    copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    SOFTWARE.
-    """)
 
 def main():
-    st.set_page_config(layout="wide")
+    # st.set_page_config(layout="wide")
     
-    tabs = st.tabs(["Калькулятор", "Инструкция", "Лицензия"])
+    tabs = st.tabs(["Калькулятор", "Инструкция"])
     
     with tabs[0]:
         st.title("Симплекс метод")
@@ -159,8 +134,6 @@ def main():
     with tabs[1]:
         show_instructions()
     
-    with tabs[2]:
-        show_license()
 
 if __name__ == "__main__":
     main()
