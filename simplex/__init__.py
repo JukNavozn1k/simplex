1fb4ddeae9db7c1e284894a29ae0ecb9a816db8c
gleb <deffender1337@gmail.com>
2025-09-12 23:32:57 +0300

    feat: removed dual

--- simplex/__init__.py
+++ simplex/__init__.py
@@ -1,8 +1,8 @@
 from .base import simplex, SimplexResult
 from .bnb import solve_integer
-from .dual import dual_simplex
+
 from .gomory import gomory_integer
 # For backward compatibility with old UI code
 bnb = solve_integer
 
-# __all__ = ['simplex', 'SimplexResult', 'solve_integer', 'dual_simplex', 'bnb']
+# __all__ = ['simplex', 'SimplexResult', 'solve_integer', 'bnb']
